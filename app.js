c3853e4dad2e147f58285877033e13b18eabe7a8
Tommaso Moro <moroxtommaso@gmail.com>
2021-11-26 21:23:00 +0100

    terminato il form e il server

---
+++ app.js
@@ -0,0 +1,53 @@
+const Joi = require('joi')
+const path = require('path')
+const express = require('express')
+
+const db = require('./utils/database.js')
+const app = express()
+
+app.use(express.json())
+
+/*app.enable('trust proxy')
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+})*/
+
+app.get('/', (req, res) => {
+    app.use(express.static(path.join(__dirname, 'public')))
+    res.sendFile(path.join(__dirname, 'public'))
+})
+
+
+app.post('/api/problem', async (req, res) => {
+    const { error } = Joi.object({
+            email: Joi.string().email(),
+            description: Joi.string().required()
+        }).validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const result = await db.logError(req.body.email, req.body.description)
+    if (result.error) return res.status(500).send(result)
+
+    res.send(req.body)
+})
+
+app.post('/api/review', async (req, res) => {
+    const { error } = Joi.object({
+            vote: Joi.number().integer().min(1).max(5).required(),
+            review: Joi.string().required()
+        }).validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const result = await db.logReview(req.body.vote, req.body.review)
+    if (result.error) return res.status(500).send(result)
+
+    res.send(req.body)
+})
+
+
+/*app.get('*', function(req, res){
+    res.redirect('https://' + req.headers.host)
+})*/
+
+const port =  process.env.PORT || 3000
+app.listen(port, () => console.warn(`Listening on port ${port}...`))
