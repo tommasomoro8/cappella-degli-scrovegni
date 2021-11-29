e2b0369c1baf707938d060f582e2bfea3be21fac
Tommaso Moro <moroxtommaso@gmail.com>
2021-11-29 15:07:58 +0100

    ready to go online

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ app.use((req, res, next) => {
     req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
 })
 
-app.get('/', (req, res) => {
-    app.use(express.static(path.join(__dirname, 'public')))
-    res.sendFile(path.join(__dirname, 'public'))
-})
-
 
 app.post('/api/problem', async (req, res) => {
     const { error } = Joi.object({
@@ -44,7 +39,16 @@ app.post('/api/review', async (req, res) => {
     res.send(req.body)
 })
 
-//add redirect on 404
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public'))
+})
+
+
+app.get('*', (req, res) => {
+    res.redirect('https://' + req.headers.host)
+})
 
 const port =  process.env.PORT || 3000
 app.listen(port, () => console.warn(`Listening on port ${port}...`))
